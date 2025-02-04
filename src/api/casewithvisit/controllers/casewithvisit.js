920e536594ff0069b6d23d51edc708c44a73ad60
Arafat Magezi <arafats144@gmail.com>
2025-02-04 22:47:51 +0300

    Rectified the cases section-2

--- src/api/casewithvisit/controllers/casewithvisit.js
+++ src/api/casewithvisit/controllers/casewithvisit.js
@@ -1,51 +1,18 @@
-// @ts-nocheck
 'use strict';
-
-const { createCoreController } = require('@strapi/strapi').factories;
 
 /**
  * A set of functions called "actions" for `casewithvisit`
  */
 
 module.exports = {
-  // Find method
-  async find(ctx) {
-    try {
-      const entities = await strapi.entityService.findMany('api::casewithvisit.casewithvisit', {
-        ...ctx.query
-      });
-      return { data: entities };
-    } catch (error) {
-      ctx.throw(500, error);
-    }
-  },
-
-  // Create method
-  async create(ctx) {
-    try {
-      const { data } = ctx.request.body;
-      const entity = await strapi.entityService.create('api::casewithvisit.casewithvisit', {
-        data: {
-          ...data,
-          publishedAt: new Date()
-        }
-      });
-      return { data: entity };
-    } catch (error) {
-      ctx.throw(500, error);
-    }
-  },
-
-  // Add method for case and visit
-  async add(ctx) {
+  add: async (ctx) => {
     try {
       const { caseData, visitData } = ctx.request.body.data;
+      console.log('Inside case add. Data is', caseData, visitData);
 
       const newCase = await strapi.entityService.create('api::case.case', {
-        data: { 
-          ...caseData,
-          publishedAt: new Date()
-        },
+        data: { ...caseData, publishedAt: new Date() },
+       
       });
 
       const newCaseVisit = await strapi.entityService.create('api::casevisit.casevisit', {
@@ -55,63 +22,51 @@ module.exports = {
           publishedAt: new Date()
         },
       });
+      console.log('New case and visit created')
 
-      return {
-        data: {
-          message: 'Case and case visit successfully created',
-          case: newCase,
-          caseVisit: newCaseVisit,
-        }
+      ctx.body = {
+        message: 'Case and case visit successfully created',
+        case: newCase,
+        caseVisit: newCaseVisit,
+        
       };
     } catch (error) {
-      ctx.throw(400, error);
+      ctx.status = 400;
+      console.error(error);
+      ctx.body = {
+        message: 'An error occurred while creating case and case visit',
+        error: error.message,
+      };
     }
   },
 
-  // Edit method for case visits
-  async edit(ctx) {
+  edit: async (ctx) => {
     try {
-      const { visitData } = ctx.request.body.data;
+      const { visitData } = ctx.request.body.data; 
       
       const updatedCaseVisits = await Promise.all(
         Object.keys(visitData).map(async (visitId) => {
           return await strapi.entityService.update('api::casevisit.casevisit', visitId, {
             data: {
               ...visitData[visitId],
-              publishedAt: new Date()
-            },
+              publishedAt: new Date() // Ensure publishedAt is inside data
+            }
           });
         })
       );
 
-      return {
-        data: {
-          message: 'Case visit(s) successfully updated',
-          caseVisits: updatedCaseVisits,
-        }
+      ctx.body = {
+        message: 'Case visit(s) successfully updated',
+        caseVisits: updatedCaseVisits,
       };
     } catch (error) {
-      ctx.throw(400, error);
+      ctx.status = 400;
+      console.error(error);
+      ctx.body = {
+        message: 'An error occurred while updating case visit',
+        error: error.message,
+      };
     }
   },
 
-  // Update method
-  async update(ctx) {
-    try {
-      const { id } = ctx.params;
-      const { data } = ctx.request.body;
-      
-      const entity = await strapi.entityService.update('api::casewithvisit.casewithvisit', id, {
-        data: {
-          ...data,
-          publishedAt: new Date()
-        }
-      });
-      
-      return { data: entity };
-    } catch (error) {
-      ctx.throw(500, error);
-    }
-  }
 };
-
--- src/api/casewithvisit/routes/casewithvisit.js
+++ src/api/casewithvisit/routes/casewithvisit.js
@@ -1,33 +1,22 @@
-'use strict';
-
 module.exports = {
   routes: [
-    // Basic CRUD routes
-    {
-      method: 'GET',
-      path: '/casewithvisit',
-      handler: 'casewithvisit.find',
-    },
-    {
-      method: 'POST',
-      path: '/casewithvisit',
-      handler: 'casewithvisit.create',
-    },
-    {
-      method: 'PUT',
-      path: '/casewithvisit/:id',
-      handler: 'casewithvisit.update',
-    },
-    // Custom routes
-    {
-      method: 'POST',
-      path: '/casewithvisit/add',
-      handler: 'casewithvisit.add',
-    },
     {
       method: 'POST',
       path: '/casewithvisit/edit',
       handler: 'casewithvisit.edit',
+      config: {
+        policies: [],
+        middlewares: [],
+      },
+    },
+    {
+      method: 'POST',
+      path: '/casewithvisit/add',
+      handler: 'casewithvisit.add',
+      config: {
+        policies: [],
+        middlewares: [],
+      },
     },
   ],
 };
